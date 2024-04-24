bcb3ebdaaf96de46715793892779805ad41d885e
EmirhanKayapinar <103645980+EmirhanKayapinar@users.noreply.github.com>
2024-04-25 00:54:32 +0300

    Update register_controller.dart
    snackbar added register

--- lib/ui/register/register_controller.dart
+++ lib/ui/register/register_controller.dart
@@ -7,7 +7,7 @@ import 'package:plantist/routes/app_pages.dart';
 class RegisterController extends GetxController {
   RxBool isEmailValid = false.obs;
   RxBool isPasswordValid = false.obs;
-  RxBool showPassword = false.obs;
+  RxBool showPassword = true.obs;
   String password = "";
   String email = "";
   UserCredential? userCredential;
@@ -25,25 +25,26 @@ class RegisterController extends GetxController {
       debugPrint("Kayıt başarılı: ${userCredential.user!.email}");
     } on FirebaseAuthException catch (e) {
       debugPrint("Kayıt hatası: $e");
+      Get.snackbar("", "Register error: $e");
+      EasyLoading.dismiss();
     }
   }
 
   Future<void> signInWithEmailPassword(String email, String password) async {
     try {
       EasyLoading.show();
-       userCredential =
-          await FirebaseAuth.instance.signInWithEmailAndPassword(
+      userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
         email: email,
         password: password,
       );
-      
-      
+
       EasyLoading.dismiss();
 
       Get.toNamed(AppRoutes.home);
       debugPrint("Giriş başarılı: ${userCredential?.user!.email}");
     } on FirebaseAuthException catch (e) {
-      debugPrint("Giriş hatası: $e");
+      Get.snackbar("", "Incorrect email or password");
+      EasyLoading.dismiss();
     }
   }
 }
